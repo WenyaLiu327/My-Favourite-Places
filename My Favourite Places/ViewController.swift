abd392c45a80c66c4bbf71aa63bda44fdce9224e
Dirk HU <hszdh@outlook.com>
2023-12-03 22:50:06 +0800

    Initial Commit

---
+++ My Favourite Places/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Favourite Places
+//
+//  Created by 胡思哲 on 2023/12/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
